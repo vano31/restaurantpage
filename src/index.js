98dc9fdaab9e9356be17b409281b1e84ecc4e74a
vano31 <vanosike31@gmail.com>
2023-02-14 20:32:23 -0500

    Made menu.js file for menu tab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+import {menu} from './menu.js'
+
+
+console.log('My name is Skylar White, yo')
+
+let title = document.createElement(`div`);
+title.textContent = 'Welcome to Sanji\'s Sea Store! ';
+document.body.appendChild(title);
+
+
+let description = document.createElement(`div`);
+description.textContent = 'Sanji is the premiere chef of the Straw Hat Pirates! With him as your cook, you\'ll be bound to eat all sorts of worldwide delicacies, from the East Blue to the New World!'
+document.body.appendChild(description);
+
+let hours = document.createElement(`div`);
+hours.textContent = `List of Hours: 
+Sunday: 8am - 8pm 
+Monday to Friday: 12pm - 4am 
+Saturday: 1pm - 4am`
+document.body.appendChild(hours);
+
+let place = document.createElement('div');
+place.textContent = 'Located on the Thousand Sunny, the pirate ship of the Straw Hat Pirates, led by Yonko Straw Hat Luffy';
+document.body.appendChild(place);
+
+//menu();
 
---
+++ src/menu.js
@@ -0,0 +1,9 @@
+
+let menu = function() {
+
+    console.log('My name is Jessie Pinkman, yo');
+
+}
+
+export {menu};
+
